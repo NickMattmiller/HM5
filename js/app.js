3b8a1003831af3e0713c468c0e7927dc3e68f687
NickMattmiller <njmatt03@louisville.edu>
2015-02-04 16:14:34 -0500

    3.6 commit

--- js/app.js
+++ js/app.js
@@ -8,21 +8,32 @@
   app.controller('TabController', function(){
     this.tab = 1;
 
-    this.setTab = function(newValue){
-      this.tab = newValue;
+    this.setTab = function(tab){
+      this.tab = tab;
     };
 
-    this.isSet = function(tabName){
-      return this.tab === tabName;
+    this.isSet = function(tab){
+      return (this.tab === tab);
     };
   });
 
   app.controller('GalleryController', function(){
     this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
+
+    this.setCurrent = function(index){
+      this.current = index;
     };
   });
+
+app.controller('ReviewController', function() {
+  this.review = {};
+
+  this.addReview = function(product) {
+    product.reviews.push(this.review);
+
+    this.review = {};
+  };
+});
 
   var gems = [
     {
@@ -49,8 +60,7 @@
         author: "tim@example.org",
         createdOn: 1397490980837
       }]
-    },
-    {
+    }, {
       name: 'Bloodstone',
       description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
       shine: 9,
@@ -74,8 +84,7 @@
         author: "gemsRock@example.org",
         createdOn: 1397490980837
       }]
-    },
-    {
+    }, {
       name: 'Zircon',
       description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
       shine: 70,
@@ -86,7 +95,7 @@
       images: [
         "images/gem-06.gif",
         "images/gem-07.gif",
-        "images/gem-09.gif"
+        "images/gem-08.gif"
       ],
       reviews: [{
         stars: 1,
@@ -100,7 +109,7 @@
         createdOn: 1397490980837
       }, {
         stars: 1,
-        body: "Don't waste your rubles!!!!",
+        body: "Don't waste your rubles!",
         author: "nat@example.org",
         createdOn: 1397490980837
       }]
